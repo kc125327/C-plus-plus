af683890aec0b50713b269a9f013c94e4af4443e
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-03-29 09:39:17 -0500

    Update

---
+++ ExpensiveCalculator.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+int a;
+
+int main()
+{
+	int aFirst, bSecond, cAdd, dSubtract;
+	std::string nameString;
+	std::cout << "What is your name: ";
+	std::cin >> nameString;
+
+	std::cout << "Enter a Number: ";
+	std::cin >> bSecond;
+
+	cAdd = aFirst + bSecond;
+	dSubtract = aFirst - bSecond;
+
+	//Divide =
+	//Multiply = 
+	//Remainder =
+	std::cout << " and " << bSecond << " = " << cAdd << std::endl;
+	//Show out put here for each
+	std::cout << "The End " << nameString << std::endl;
+
+}
