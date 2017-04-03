d524e96bd7f6d7fcf53be93337d417f2c709b783
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-03 11:10:11 -0500

    Update

--- Temperture/Temperture/Source.cpp
+++ Temperture/Temperture/Source.cpp
@@ -10,12 +10,28 @@ int main()
 	std::cout << "What was the Temperture (celsius) at 8:00 A.M? : " << std::endl;
 	std::cin>> cel8;
 
+	if (cel8 > 10)
+	{
+		std::cout << "Error: Can't give a temperture higher than the first";
+
+	}
 	std::cout << "What was the Temperture (celsius) at 12:00 P.M? : " << std::endl;
 	std::cin>> cel12;
+	if (cel12 > 10)
+	{
+		std::cout << "Error: Can't give a temperture higher than the first";
 
+	}
 
 	std::cout << "What was the Temperture (celsius) at 5:00 P.M? : " << std::endl;
 	std::cin >> cel5;
+	if (cel5 > 10)
+	{
+		std::cout << "Error: Can't give a temperture higher than the first";
+
+	}
+	system  ("pause");
+	return 0;
 
 
 	far8 = cel8 * 9/5 + 32;
