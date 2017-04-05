a490feee7503fb2b23d6ac231bcded36ae397763
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-05 09:56:31 -0500

    Update

--- ExpensiveCalculator/ExpensiveCalculator/Source.cpp
+++ ExpensiveCalculator/ExpensiveCalculator/Source.cpp
@@ -5,22 +5,25 @@ int a;
 
 int main()
 {
-	int aFirst, bSecond, cAdd, dSubtract;
+	int aFirst, bSecond, cAdd, dSubtract, eDivide, fMultiply, gRemainder;
 	std::string nameString;
 	std::cout << "What is your name: ";
 	std::cin >> nameString;
 
 	std::cout << "Enter a Number: ";
+	std::cin >> aFirst;
+
+	std::cout << "Enter another Number: ";
 	std::cin >> bSecond;
 
+	//The math
 	cAdd = aFirst + bSecond;
 	dSubtract = aFirst - bSecond;
-
-	//Divide =
-	//Multiply = 
-	//Remainder =
+	Divide = aFirst / bSecond;
+	Multiply = aFirst * bSecond;
+	Remainder = aFirst % bSecond;
 	std::cout << " and " << bSecond << " = " << cAdd << std::endl;
-	//Show out put here for each
+
 	std::cout << "The End " << nameString << std::endl;
 
 }
