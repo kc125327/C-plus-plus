0ffdc5b79ed7a2b639089315d5f22675935cc880
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-05 09:44:09 -0500

    Update

---
+++ Debugging/Debugging/Source.cpp
@@ -0,0 +1,24 @@
+
+//SwatTheBugs16.cpp
+//Created/revised by 
+//Kayla Clayton 
+// 4/5/2017
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//declare variable
+	double temp = 35.3;
+
+	//increase variable's value by 1.5
+	temp = temp + 1.5;
+
+		//display variable's value
+		std::cout << "After adding 1.5, the temp variable now contains " << temp << "." << endl;
+
+	system("pause");
+	return 0;
+}	//end of main function
