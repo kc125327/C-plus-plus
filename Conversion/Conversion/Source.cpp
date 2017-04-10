4c08ad6cc8b10c8baa506ac2e4897c8324abbcc0
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-10 11:00:55 -0500

    Update

---
+++ Conversion/Conversion/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double american, japanese, mexico, europe;
+
+	std::cout << "How much money do you have? " << std::endl;
+	std::cin >> american;
+
+	japanese = american * 82.34;
+	mexico = 
+
+}
