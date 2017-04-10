b03dc3e0e374ecf2e8b0dfc20e9dd4b1e3f4ea4e
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-10 11:09:54 -0500

    Update

--- Conversion/Conversion/Source.cpp
+++ Conversion/Conversion/Source.cpp
@@ -9,6 +9,11 @@ int main()
 	std::cin >> american;
 
 	japanese = american * 82.34;
-	mexico = 
+	mexico = american * 12.8863;
+	europe = american * .6318;
+
+
+	std::cout << "You came in with $" << american << " dollars. We have converted them into Pesos, Pounds, and Yen." << std::endl;
+	std::cout << "For Pesos you have " << mexico << ". For Pounds you have " << europe << ". For Yen you have " << japanese << std::endl;
 
 }
