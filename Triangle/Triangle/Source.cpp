31bca5dc2d3b16cd3e189e36c3ace5dd2245d946
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-13 10:28:52 -0500

    Update

--- Triangle/Triangle/Source.cpp
+++ Triangle/Triangle/Source.cpp
@@ -24,36 +24,100 @@ int main()
 			//do math here
 			//Display answer here
 		}
-		if (chooseSide == "B")
+		if (chooseSide == "B" || chooseSide == "b)
 		{
-			//enter side B
-			//enter side C
+			std::cout << "Enter side A";
+			std::cin >> sideA;
+
+			std::cout << "Enter side C";
+			std::cin >> sideC;
 			//do math here
 			//Display answer here
 		}
-	}
+		if (chooseSide == "C" || chooseSide == "c")
+		{
+			std::cout << "Enter side B";
+			std::cin >> sideB;
+
+			std::cout << "Enter side A";
+			std::cin >> sideA;
+
+		}
 	
 
-	if (triangleChoice == "Isoceles" || triangleChoice == "yes")
+	if (triangleChoice == "Isosceles" || triangleChoice == "isosceles")
 	{
+
+		std::cout << "Please enter a side that you are trying to solve";
+		std::cin >> chooseSide;
+		if (chooseSide == "A" || chooseSide == "a")
+		{
+			std::cout << "Please enter for side B";
+			std::cin >> sideB;
+			std::cout << "Please enter for side C";
+			std::cin >> sideC;
+			\
+				//do math here
+				//Display answer here
+		}
+		if (chooseSide == "B" || chooseSide == "b)
+		{
+			std::cout << "Enter side A";
+			std::cin >> sideA;
+
+			std::cout << "Enter side C";
+			std::cin >> sideC;
+			//do math here
+			//Display answer here
+		}
+		if (chooseSide == "C" || chooseSide == "c")
+		{
+			std::cout << "Enter side B";
+			std::cin >> sideB;
+
+			std::cout << "Enter side A";
+			std::cin >> sideA;
+
+
+	}
+
+
+	if (triangleChoice == "Equilateral" || triangleChoice == "equilateral")
+	{
+		std::cout << "Please enter a side that you are trying to solve";
+		std::cin >> chooseSide;
+		if (chooseSide == "A" || chooseSide == "a")
+		{
+			std::cout << "Please enter for side B";
+			std::cin >> sideB;
+			std::cout << "Please enter for side C";
+			std::cin >> sideC;
+			\
+				//do math here
+				//Display answer here
+		}
+		if (chooseSide == "B" || chooseSide == "b)
+		{
+			std::cout << "Enter side A";
+			std::cin >> sideA;
+
+			std::cout << "Enter side C";
+			std::cin >> sideC;
+			//do math here
+			//Display answer here
+		}
+		if (chooseSide == "C" || chooseSide == "c")
+		{
+			std::cout << "Enter side B";
+			std::cin >> sideB;
+
+			std::cout << "Enter side A";
+			std::cin >> sideA;
 
 
 
 	}
 
-	std::cout << "Do you have a equilateral? Yes or No?" << std::endl;
-	if (equilateral == "Yes" || equilateral == "yes")
-	{
-
-
-
-	}
-	else
-	{
-
-
-
-	}
 
 
 }
