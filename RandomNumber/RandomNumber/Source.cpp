b5cc9fa7e771396dbda3792630e8aafa2b463ab7
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-04-18 11:19:41 -0500

    uPDATE

--- RandomNumber/RandomNumber/Source.cpp
+++ RandomNumber/RandomNumber/Source.cpp
@@ -4,41 +4,42 @@
 
 int main()
 {
-	srand(static_cast<unsigned int>(time(0)));
-	int randomNumber = rand();
-	int die = (randomNumber % 100) + 1;
-	int number;
-	int chances = 0;
+	
+		int number;
+		int chances = 0;
 
-	std::string secondChance;
-
-	do{
-		chances++;
-		std::cout << "Pick a number between 1 and 6" << std::endl;
+	
+		std::cout << "Pick a number between 1 and 10" << std::endl;
 		std::cin >> number;
+		do {
+			chances = chances + 1;
+			srand(static_cast<unsigned int>(time(0)));
+			int randomNumber = rand();
+			int die = (randomNumber % 10) + 1;
+			std::cout << "The number the computer chose was " << die << std::endl;
 
-		if (number == die)
-		{
-			std::cout << "Congratulations the number " << die << " is correct!" << std::endl;
+			if (die == number)
+			{
+				std::cout << "Congratulations the number " << number << " is correct!" << std::endl;
 
-		}
-		else if (number > die){
-
-
-			std::cout << "You chose too high. " << std::endl;
-		}
-		else(number < die){
+			}
+			else if (die > number){
 
 
-			std::cout << "You chose too low. " << std::endl;
-		}
+				std::cout << "You chose too high. " << std::endl;
+			}
+			else{
+				std::cout << "To High " << std::endl;
+			}
 
-		std::cout << "You have used " << chances << "of 5 chances" << std::endl;
+			std::cout << "You have used " << chances << "of 5 chances" << std::endl;
+
+
+
+			//std::cout << "Game Over!" << chances << " of 5 Chances" << std::endl;
+		} while (chances == 5);
+
 		
-	} while (chances  != 5 || number == die);
-
-	std::cout << "Game Over!" << chances << " of 5 Chances" << std::endl;
-
 	system("pause");
 	return 0;
 
